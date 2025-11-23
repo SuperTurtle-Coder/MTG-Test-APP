b4be59ec24f59a732fabbe4ac8191f2a4a508339
SuperTurtle-Coder <tmntwalshn@gmail.com>
2025-11-23 10:43:24 -0500

    added scryfall api and basic html front end

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from urllib.parse import quote
+import requests
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, MTG!"}
+@app.get("/card/{name}")
+def user_input_name(name):
+    card_name = "https://api.scryfall.com/cards/named?exact=" + quote(name)
+    response = requests.get(card_name)
+    data = response.json()
+
+    return {
+    "name": data["name"],
+    "mana_cost": data["mana_cost"],
+    "type_line": data["type_line"],
+    "image_url": data["image_uris"]["large"]
+    }
+
+from fastapi.staticfiles import StaticFiles
+
+app.mount("/", StaticFiles(directory=".", html=True), name="static")
+# @app.get("/")
+# def read_root():
+#     return {"message": "Hello, MTG!"}
+
